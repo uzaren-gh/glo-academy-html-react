a98763d3896b77bb58c5bbe9067094db79f4dda1
Sergey Burenkov <uzaren@ukr.net>
2025-12-28 22:12:49 +0200

    Lesson 6. React (Start). Simple version.

---
+++ abito-app/src/constants.js
@@ -0,0 +1,51 @@
+export const cardArray = [
+    {
+        id: 0,
+        title: 'Первый товар',
+        price: '170 ₽',
+        address: 'Казань, р-н Вахитовский',
+        date: '10 июля 11:39',
+        img: '/image/card-img.png'
+    },
+    {
+        id: 1,
+        title: 'Второй товар',
+        price: '171 ₽',
+        address: 'Казань, р-н Вахитовский',
+        date: '10 июля 11:39',
+        img: '/image/card-img.png'
+    },
+    {
+        id: 2,
+        title: 'Третий товар',
+        price: '172 ₽',
+        address: 'Казань, р-н Вахитовский',
+        date: '10 июля 11:39',
+        img: '/image/card-img.png'
+    },
+    {
+        id: 3,
+        title: 'Четвертый товар',
+        price: '173 ₽',
+        address: 'Казань, р-н Вахитовский',
+        date: '10 июля 11:39',
+        img: '/image/card-img.png'
+    },
+    {
+        id: 4,
+        title: 'Пятый товар',
+        price: '174 ₽',
+        address: 'Казань, р-н Вахитовский',
+        date: '10 июля 11:39',
+        img: '/image/card-img.png'
+    },
+    {
+        id: 5,
+        title: 'Шестой товар',
+        price: '175 ₽',
+        address: 'Казань, р-н Вахитовский',
+        date: '10 июля 11:39',
+        img: '/image/card-img.png'
+    }
+
+]
